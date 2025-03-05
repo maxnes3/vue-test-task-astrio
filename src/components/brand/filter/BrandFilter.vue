1016e94fca10edccf868e52f5dbd7bac29868a1f
maxnes3 <maxnes332@gmail.com>
2025-03-05 04:02:06 +0400

    feature: change style

--- src/components/brand/filter/BrandFilter.vue
+++ src/components/brand/filter/BrandFilter.vue
@@ -2,10 +2,16 @@
   <aside>
     <h2>Brands</h2>
     <ul>
-      <li @click="handleSetFilter('all')">All Brands</li>
+      <li
+        :class="{ [$style.active]: selectedBrand === 'all' }"
+        @click="handleSetFilter('all')"
+      >
+        All Brands
+      </li>
       <li
         v-for="brand in brandStore.brands"
         :key="brand.id"
+        :class="{ [$style.active]: selectedBrand === Number(brand.id) }"
         @click="handleSetFilter(Number(brand.id))"
       >
         {{ brand.title }}
@@ -16,12 +22,15 @@
 
 <script lang="ts" setup>
 import { useBrandStore, useProductStore } from '@/store';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
+
+const selectedBrand = ref<number | 'all'>('all');
 
 const brandStore = useBrandStore();
 const productStore = useProductStore();
 
 const handleSetFilter = (brandId: number | 'all') => {
+  selectedBrand.value = brandId;
   productStore.handleFilterProductsByBrand(brandId);
 };
 
@@ -32,4 +41,19 @@ onMounted(() => {
 
 <style module lang="scss">
 @import './styles.module.scss';
+
+.active {
+  display: flex;
+  font-weight: bold;
+  color: #42b883aa;
+  align-items: center;
+  filter: drop-shadow(0 0 2em #42b883aa);
+
+  &::before {
+    content: '>';
+    font-size: 32px;
+    color: #42b883aa;
+    margin-right: 8px;
+  }
+}
 </style>
